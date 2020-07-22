37d7e70bd9d00f3cbfbcf81520702964c8fa38c5
Nastya <vol4ara2113@gmail.com>
2020-07-22 17:21:57 +0300

    22.07

--- request.js
+++ request.js
@@ -1,7 +1,7 @@
 const request = require('request');
 request.get('http://api.icndb.com/jokes/random/3', 
 (data, err, body) => {
-  //console.log(JSON.parse(body));
+  console.log(JSON.parse(body));
    let parsed= JSON.parse(body).value;
 
  let arr = [];
--- test.js
+++ test.js
@@ -1,1 +1,17 @@
+const request = require('request');
+request.get('http://www.7timer.info/bin/api.pl?lon=43.9360589&lat=56.2965039&product=astro&output=json', 
+(data, err, body) => {
+  // console.log(JSON.parse(body).dataseries);
+ let parsed= JSON.parse(body).dataseries;
 
+let arr = [];
+
+    parsed.map((elem) => { 
+           arr.push(elem.temp2m);
+                  });
+ arr.map(elem => { 
+console.log(elem);
+      console.log("");
+             });
+  }
+);
